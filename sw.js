32a34d9a6cb371eda37823dc69191a964be8d963
take-Git623 <myoujuinbon@gmail.com>
2026-02-15 18:28:00 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', e => e.waitUntil(
+  caches.open('v1').then(cache => cache.addAll([
+    'index.html','manifest.json','icon-192.png','icon-512.png'
+  ]))
+));
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
